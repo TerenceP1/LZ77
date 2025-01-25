9a0670ffe12a6c6719620df492717e59ce9b154a
TerenceP1 <158529801+TerenceP1@users.noreply.github.com>
2025-01-25 16:25:01 -0500

    Update lz77_win.cpp

--- cmake/lz77_win.cpp
+++ cmake/lz77_win.cpp
@@ -1,4 +1,17 @@
 // Implements LZ77
 
 #include <iostream>
+#include <Windows.h>
+using namespace std;
 
+int main() {
+  string file;
+  cout << "Enter name of file to compress:";
+  cin >> file;
+  HANDLE hFile = CreateFileA(file.c_str(),
+    GENERIC_READ,
+    FILE_SHARE_READ,
+    NULL,
+    OPEN_ALWAYS,
+    NULL, NULL);
+}
